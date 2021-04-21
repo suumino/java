c96000b6cbb3595d14ef10d22f3f0c7c75f75095
suumino <78770211+suumino@users.noreply.github.com>
2021-04-21 17:17:06 +0900

    Delete FlowEx24.java

--- FlowEx24.java
+++
@@ -1,9 +0,0 @@
-public class FlowEx24 {
-    public static void main(String[] args) {
-        while (i-- != 0) {
-            System.out.println(i);
-            for (int j = 0; j <2_000_000_000; j++);
-
-        }
-    }
-}
